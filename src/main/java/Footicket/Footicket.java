3700f27f811ceffc2fdca35680788d08d958ed47
Tommaso4497 <97950560+Tommaso4497@users.noreply.github.com>
2022-06-15 23:37:49 +0200

    delete space in Footicket

--- src/main/java/Footicket/Footicket.java
+++ src/main/java/Footicket/Footicket.java
@@ -10,7 +10,7 @@ import javax.swing.JFrame;
 public class Footicket extends JFrame
 {
     public static void main(final String[] args) {
-         MainFrame mainFrame = new MainFrame();
+        MainFrame mainFrame = new MainFrame();
         mainFrame.setVisible(true);
     }
 }
